8fb9578cdeb8d6a5bc79a9e24fd079209f44a930
Alan Metzger <alanm@exaqservices.com>
2025-07-27 20:08:41 -0600

    Initial Commit

---
+++ Mosaic/MosaicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MosaicApp.swift
+//  Mosaic
+//
+//  Created by Alan Metzger on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MosaicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
